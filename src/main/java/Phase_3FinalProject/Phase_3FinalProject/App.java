82f71a8f8f4ca2111486342973d288a885e7e824
swethaandrews <andrews@gmail.com>
2023-11-10 19:00:56 +0530

    Course End Project Uploaded

---
+++ src/main/java/Phase_3FinalProject/Phase_3FinalProject/App.java
@@ -0,0 +1,13 @@
+package Phase_3FinalProject.Phase_3FinalProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
